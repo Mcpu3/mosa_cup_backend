78db72e4361d8848cfc83c0cb0075ae234b20d7f
honnyama <kindfly0210@gmail.com>
2023-06-30 17:48:40 +0900

    フォームのDB関連を追加#6

--- mosa_cup_backend/api/v1/database/app.py
+++ mosa_cup_backend/api/v1/database/app.py
@@ -25,3 +25,50 @@ class User(database.Model):
     created_at = database.Column(database.DateTime, nullable=False)
     updated_at = database.Column(database.DateTime, nullable=True)
     deleted = database.Column(database.Boolean, default=False, nullable=False)
+
+class Form(database.Model):
+    __tablename__ = "Forms"
+
+    form_uuid = database.Column(database.String(48),primary_key=True)
+    board_uuid  = database.Column(database.String,nullable=False)
+    subboard_uuid = database.Column(database.String,nullable=False)
+    title = database.Column(database.String,nullable=False)
+    send_time = database.Column(database.DateTime,nullable=False)
+    scheduled_sending_time = database.Column(database.DateTime,nullable=True)
+    created_at = database.Column(database.DateTime,nullable=False)
+    updated_at = database.Column(database.DateTime,nullable=True)
+    deleted = database.Column(database.Boolean,default=False,nullable=False)
+
+class FormYesNoQuestion(database.Model):
+    __tablename__ = "FormYesNoQuestions"
+
+    form_question_uuid = database.Column(database.String(48),primary_key=True)
+    form_uuid = database.Column(database.String,nullable=False)
+    title = database.Column(database.String,nullable=False)
+    yes = database.Column(database.Boolean,nullable=False)
+    no = database.Column(database.Boolean,nullable=False)
+    created_at = database.Column(database.DateTime,nullable=False)
+    updated_at = database.Column(database.DateTime,nullable=True)
+    deleted = database.Column(database.Boolean,default=False,nullable=False)
+
+class FormResponse(database.Model):
+    __tablename__ = "FormResponses"
+
+    form_response_uuid = database.Column(database.String(48),primary_key=True)
+    form_uuid = database.Column(database.String,nullable=False)
+    user_uuid = database.Column(database.String,nullable=False)
+    created_at = database.Column(database.DateTime,nullable=False)
+    updated_at = database.Column(database.DateTime,nullable=True)
+    deleted = database.Column(database.Boolean,default=False,nullable=False)
+
+class FormYesNoQuestionResponses(database.Model):
+    __tablename__ = "FormYesNoQuestionResponses"
+
+    form_question_response_uuid = database.Column(database.String(48),primary_key=True)
+    form_response_uuid = database.Column(database.String,nullable=False)
+    form_question_uuid = database.Column(database.String,nullable=False)
+    yes = database.Column(database.Boolean,nullable=False)
+    no = database.Column(database.Boolean,nullable=False)
+    created_at = database.Column(database.DateTime,nullable=False)
+    updated_At = database.Column(database.DateTime,nullable=True)
+    deleted = database.Column(database.Boolean,default=False,nullable=False)
--- mosa_cup_backend/api/v1/models.py
+++ mosa_cup_backend/api/v1/models.py
@@ -13,3 +13,50 @@ class User(Base):
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=True)
     deleted = Column(Boolean, default=False, nullable=False)
+
+class Form(Base):
+    __tablename__ = "Forms"
+
+    form_uuid = Column(String(48),primary_key=True)
+    board_uuid  = Column(String,nullable=False)
+    subboard_uuid = Column(String,nullable=False)
+    title = Column(String,nullable=False)
+    send_time = Column(DateTime,nullable=False)
+    scheduled_sending_time = Column(DateTime,nullable=True)
+    created_at = Column(DateTime,nullable=False)
+    updated_at = Column(DateTime,nullable=True)
+    deleted = Column(Boolean,default=False,nullable=False)
+
+class FormYesNoQuestion(Base):
+    __tablename__ = "FormYesNoQuestions"
+
+    form_question_uuid = Column(String(48),primary_key=True)
+    form_uuid = Column(String,nullable=False)
+    title = Column(String,nullable=False)
+    yes = Column(Boolean,nullable=False)
+    no = Column(Boolean,nullable=False)
+    created_at = Column(DateTime,nullable=False)
+    updated_at =Column(DateTime,nullable=True)
+    deleted = Column(Boolean,default=False,nullable=False)
+
+class FormResponse(Base):
+    __tablename__ = "FormResponses"
+
+    form_response_uuid = Column(String(48),primary_key=True)
+    form_uuid = Column(String,nullable=False)
+    user_uuid = Column(String,nullable=False)
+    created_at = Column(DateTime,nullable=False)
+    updated_at = Column(DateTime,nullable=True)
+    deleted = Column(Boolean,default=False,nullable=False)
+
+class FormYesNoQuestionResponses(Base):
+    __tablename__ = "FormYesNoQuestionResponses"
+
+    form_question_response_uuid = Column(String(48),primary_key=True)
+    form_response_uuid = Column(String,nullable=False)
+    form_question_uuid = Column(String,nullable=False)
+    yes = Column(Boolean,nullable=False)
+    no = Column(Boolean,nullable=False)
+    created_at = Column(DateTime,nullable=False)
+    updated_At = Column(DateTime,nullable=True)
+    deleted = Column(Boolean,default=False,nullable=False)
